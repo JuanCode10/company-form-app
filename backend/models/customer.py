67ef6e4c111bddeed9579262d91d6aeef558275c
JuanCode10 <juancode.dev@gmail.com>
2026-08-04 23:17:46 -0600

    feat(backend): implement customer service helpers and validation logic

--- backend/models/customer.py
+++ backend/models/customer.py
@@ -1,13 +1,13 @@
 from db import db
 from datetime import datetime, timezone
 
-class Customer(db.Model):
+class CustomerModel(db.Model):
     __tablename__ = "customers"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=False, nullable=False)
     phone_number = db.Column(db.String(14), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), nullable=True)
     created_at = db.Column(
         db.DateTime(timezone=True), 
         nullable=False, 
@@ -19,3 +19,5 @@ class Customer(db.Model):
         default=datetime.now(timezone.utc),
         onupdate=datetime.now(timezone.utc),
     )
+
+    # Relationships
